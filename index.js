302072ea13862c94b1b689a3b336821ac4f785dc
Adriel Modollo <adrielmodollo@outlook.com>
2021-01-11 21:04:11 -0300

    teste

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ n3 = parseFloat(readline.question("Informe a terceira nota:"));
 n4 = parseFloat(readline.question("Informe a quarta nota:"));
 
 media = (n1+n2+n3+n4)/4;
-
+//teste
 if (n1< 0 || n1 > 10 || n2 < 0 || n2 > 10 || n3 < 0 || n3 > 10 || n4 < 0 || n4 > 10) {
     console.log("Ops, parece que você informou alguma nota errada!")
 } else {
